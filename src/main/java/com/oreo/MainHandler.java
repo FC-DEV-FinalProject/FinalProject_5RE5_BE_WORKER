34fc114f6993e8eee795365cd26f793056350c40
oowtl <oowtl7425@gmail.com>
2024-12-02 19:47:19 +0900

    [feat] 임시 기능 - sqs 리턴

--- src/main/java/com/oreo/MainHandler.java
+++ src/main/java/com/oreo/MainHandler.java
@@ -1,28 +1,61 @@
 package com.oreo;
 
+import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent.MessageAttribute;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+import com.amazonaws.services.sqs.AmazonSQSResponder;
+import com.amazonaws.services.sqs.AmazonSQSResponderClientBuilder;
+import com.amazonaws.services.sqs.MessageContent;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.MessageAttributeValue;
 
 public class MainHandler implements RequestHandler<SQSEvent, String> {
 
     @Override
     public String handleRequest(SQSEvent event, Context context) {
+        AmazonSQSResponder sqs = AmazonSQSResponderClientBuilder.defaultClient();
+
         for (SQSMessage msg : event.getRecords()) {
-            processMessage(msg, context);
+            processMessage(sqs, msg, context);
         }
 
         return "Hello from Lambda!";
     }
 
-    private void processMessage(SQSMessage event, Context context) {
-        try {
-            context.getLogger().log("Received event: " + event);
+    private void processMessage(AmazonSQSResponder sqs, SQSMessage sqsMessage, Context context) {
 
-        } catch (Exception e) {
-            context.getLogger().log(e.getMessage());
-        }
+        System.out.println("sqsMessage.getBody() = " + sqsMessage.getBody());
+        System.out.println("context.getFunctionName() = " + context.getFunctionName());
+        context.getLogger();
+
+        Message message = Message.builder()
+            .body(sqsMessage.getBody())
+            .messageAttributes(sqsMessage.getMessageAttributes().entrySet().stream().collect(
+                Collectors.toMap(Entry::getKey,
+                    entry -> toMessageAttributeValue(entry.getValue()))))
+            .build();
+
+        MessageContent requestMessage = MessageContent.fromMessage(message);
+
+        System.out.println("message.getBody() = " + message.body());
+
+        MessageContent response = new MessageContent(sqsMessage.getBody());
+
+        sqs.sendResponseMessage(requestMessage, response);
+
+    }
+
+    private MessageAttributeValue toMessageAttributeValue(MessageAttribute attribute) {
+        return MessageAttributeValue.builder()
+            .dataType(attribute.getDataType())
+            .stringValue(attribute.getStringValue())
+            .stringListValues(attribute.getStringListValues())
+//            .binaryListValues(attribute.getBinaryListValues())
+            .build();
     }
 
 }
