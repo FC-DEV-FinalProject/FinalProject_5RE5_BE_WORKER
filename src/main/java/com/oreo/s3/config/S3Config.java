d6234d3dda21bcf955546f0010af8775d8574aac
alswooy <108874185+alswooy@users.noreply.github.com>
2024-12-03 17:44:33 +0900

    [feat] : s3 upload, load 추가 - s3 upload 추가 - env 설정 변경

--- src/main/java/com/oreo/s3/config/S3Config.java
+++ src/main/java/com/oreo/s3/config/S3Config.java
@@ -5,12 +5,13 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class S3Config {
-    private static final String AWS_S3_ACCESSKEY = System.getenv("AWS_S3_ACCESSKEY");
-    private static final String AWS_S3_BUKET_NAME = System.getenv("AWS_S3_BUKET_NAME");
-    private static final String AWS_S3_REGION = System.getenv("AWS_S3_REGION");
-    private static final String AWS_S3_SECRETKEY = System.getenv("AWS_S3_SECRETKEY");
+    private static Dotenv dotenv = Dotenv.load();
+    private static final String AWS_S3_ACCESSKEY = dotenv.get("AWS_S3_ACCESSKEY");
+    private static final String AWS_S3_REGION = dotenv.get("AWS_S3_REGION");
+    private static final String AWS_S3_SECRETKEY = dotenv.get("AWS_S3_SECRETKEY");
     public static AmazonS3 amazonS3Client() {
         // AWS 자격 증명 객체 생성
         BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(AWS_S3_ACCESSKEY, AWS_S3_SECRETKEY);
