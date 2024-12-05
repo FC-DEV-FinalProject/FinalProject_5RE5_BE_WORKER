68c9ae88799c6bd60c648b1e7418f8db768c5fc3
oowtl <oowtl7425@gmail.com>
2024-12-05 15:42:16 +0900

    [feat] sqs mainhandler 에서 라우팅 기능 추가

--- src/main/java/com/oreo/MainHandler.java
+++ src/main/java/com/oreo/MainHandler.java
@@ -8,12 +8,23 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
 import com.amazonaws.services.sqs.AmazonSQSResponder;
 import com.amazonaws.services.sqs.AmazonSQSResponderClientBuilder;
 import com.amazonaws.services.sqs.MessageContent;
+import com.oreo.tts.dto.AudioOptionDto;
+import com.oreo.tts.dto.TtsSentenceDto;
+import com.oreo.tts.dto.VoiceDto;
+import com.oreo.tts.dto.request.TtsMakeRequest;
+import com.oreo.tts.dto.response.TtsMakeResponse;
+import com.oreo.tts.service.TtsMakeService;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.services.sqs.model.Message;
 import software.amazon.awssdk.services.sqs.model.MessageAttributeValue;
 
 public class MainHandler implements RequestHandler<SQSEvent, String> {
+
+    private static final Logger log = LoggerFactory.getLogger(MainHandler.class);
+    private static final TtsMakeService ttsMakeService = TtsMakeService.getInstance();
 
     @Override
     public String handleRequest(SQSEvent event, Context context) {
@@ -42,12 +53,39 @@ public class MainHandler implements RequestHandler<SQSEvent, String> {
         MessageContent requestMessage = MessageContent.fromMessage(message);
 
         System.out.println("message.getBody() = " + message.body());
+        Object routerResponse  = router(message);
+        log.info("routerreponse : {} ", routerResponse);
+
+        log.info("messageType: {}", message.attributes().get("messageType"));
 
         MessageContent response = new MessageContent(sqsMessage.getBody());
 
         sqs.sendResponseMessage(requestMessage, response);
 
     }
+
+    private Object router(Message message) {
+        String messageType = message.attributes().get("messageType");
+
+        if (messageType.equals("TTS_MAKE")) {
+
+            VoiceDto voiceD = new VoiceDto("ko-KR", "ko-KR-Standard-C", "female");
+            AudioOptionDto audioOptionD = new AudioOptionDto(1.0f, 0.0f, 100);
+            TtsSentenceDto ttsSentenceDto = new TtsSentenceDto("안녕하세요", voiceD, audioOptionD);
+            TtsMakeRequest ttsMakeRequest = new TtsMakeRequest(ttsSentenceDto, "lambdaTest");
+
+            log.info("ttsMakeRequest : {} ", ttsMakeRequest);
+
+            TtsMakeResponse ttsMakeResponse = ttsMakeService.makeTtsAndUploadS3(ttsMakeRequest);
+
+            log.info("ttsMakeResponse : {} ", ttsMakeResponse);
+            return ttsMakeResponse;
+        }
+
+
+        throw new IllegalArgumentException("Unknown message type: " + messageType);
+    }
+
 
     private MessageAttributeValue toMessageAttributeValue(MessageAttribute attribute) {
         return MessageAttributeValue.builder()
