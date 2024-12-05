5611ba06679964900fe011dac388c82490945903
oowtl <oowtl7425@gmail.com>
2024-12-05 18:39:14 +0900

    [fix] sqs serializer 테스트
    - 테스트

--- src/main/java/com/oreo/MainHandler.java
+++ src/main/java/com/oreo/MainHandler.java
@@ -8,6 +8,7 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
 import com.amazonaws.services.sqs.AmazonSQSResponder;
 import com.amazonaws.services.sqs.AmazonSQSResponderClientBuilder;
 import com.amazonaws.services.sqs.MessageContent;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oreo.tts.dto.AudioOptionDto;
 import com.oreo.tts.dto.TtsSentenceDto;
@@ -15,6 +16,8 @@ import com.oreo.tts.dto.VoiceDto;
 import com.oreo.tts.dto.request.TtsMakeRequest;
 import com.oreo.tts.dto.response.TtsMakeResponse;
 import com.oreo.tts.service.TtsMakeService;
+import com.oreo.util.RequestSerializer;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
@@ -27,22 +30,32 @@ public class MainHandler implements RequestHandler<SQSEvent, String> {
 
     private static final Logger log = Logger.getLogger("com.oreo.MainHandler");
     private static final TtsMakeService ttsMakeService = TtsMakeService.getInstance();
+    private static final ObjectMapper mapper = RequestSerializer.getInstance();
 
     @Override
     public String handleRequest(SQSEvent event, Context context) {
         AmazonSQSResponder sqs = AmazonSQSResponderClientBuilder.defaultClient();
 
-        for (SQSMessage msg : event.getRecords()) {
-            processMessage(sqs, msg, context);
+        try {
+            for (SQSMessage msg : event.getRecords()) {
+                processMessage(sqs, msg, context);
+            }
+        } catch (
+            JsonProcessingException e) {
+            log.log(Level.SEVERE, "Error processing message", e);
         }
-
         return "Hello from Lambda!";
     }
 
-    private void processMessage(AmazonSQSResponder sqs, SQSMessage sqsMessage, Context context) {
+    private void processMessage(AmazonSQSResponder sqs, SQSMessage sqsMessage, Context context)
+        throws JsonProcessingException {
         context.getLogger();
 
         log.log(Level.INFO, "context : " + context);
+
+        Map<String, Object> sqsEventMap = mapper.readValue(sqsMessage.getBody(), Map.class);
+        log.log(Level.INFO, "sqsEventMap : " + sqsEventMap);
+
 
         Message message = Message.builder()
             .body(sqsMessage.getBody())
@@ -55,7 +68,7 @@ public class MainHandler implements RequestHandler<SQSEvent, String> {
 
         log.log(Level.INFO, "message : " + message.toString());
         log.log(Level.INFO, "message.body() : " + message.body());
-        log.log(Level.INFO, "message.messageAttributes() " + message.messageAttributes().toString());
+//        log.log(Level.INFO, "message.messageAttributes() " + message.messageAttributes().toString());
 
         Object routerResponse  = router(message);
         log.log(Level.INFO, "routerreponse: " + routerResponse);
