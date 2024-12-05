f66e6d041ffe5622e66a1932ef04f5fcb27e9a4f
alswooy <108874185+alswooy@users.noreply.github.com>
2024-12-05 20:36:01 +0900

    [fix] : handleRequest log 출력
    - log 출력
    - 예외처리 삭제

--- src/main/java/com/oreo/MainHandler.java
+++ src/main/java/com/oreo/MainHandler.java
@@ -8,7 +8,6 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
 import com.amazonaws.services.sqs.AmazonSQSResponder;
 import com.amazonaws.services.sqs.AmazonSQSResponderClientBuilder;
 import com.amazonaws.services.sqs.MessageContent;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oreo.tts.dto.AudioOptionDto;
 import com.oreo.tts.dto.TtsSentenceDto;
@@ -17,14 +16,13 @@ import com.oreo.tts.dto.request.TtsMakeRequest;
 import com.oreo.tts.dto.response.TtsMakeResponse;
 import com.oreo.tts.service.TtsMakeService;
 import com.oreo.util.RequestSerializer;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import software.amazon.awssdk.services.sqs.model.Message;
 import software.amazon.awssdk.services.sqs.model.MessageAttributeValue;
+
+import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class MainHandler implements RequestHandler<SQSEvent, String> {
 
@@ -34,36 +32,25 @@ public class MainHandler implements RequestHandler<SQSEvent, String> {
 
     @Override
     public String handleRequest(SQSEvent event, Context context) {
+        log.log(Level.SEVERE, "[handleRequest] evnet - "+ event+ " | context - " + context);
         AmazonSQSResponder sqs = AmazonSQSResponderClientBuilder.defaultClient();
-
-        try {
+        log.log(Level.SEVERE, "[handleRequest] sqs - "+ sqs);
             for (SQSMessage msg : event.getRecords()) {
+                log.log(Level.SEVERE, "[handleRequest] msg - " + msg);
                 processMessage(sqs, msg, context);
             }
-        } catch (
-            JsonProcessingException e) {
-            log.log(Level.SEVERE, "Error processing message", e);
-        }
         return "Hello from Lambda!";
     }
 
-    private void processMessage(AmazonSQSResponder sqs, SQSMessage sqsMessage, Context context)
-        throws JsonProcessingException {
+    private void processMessage(AmazonSQSResponder sqs, SQSMessage sqsMessage, Context context){
         context.getLogger();
-
         log.log(Level.INFO, "context : " + context);
-
-        Map<String, Object> sqsEventMap = mapper.readValue(sqsMessage.getBody(), Map.class);
-        log.log(Level.INFO, "sqsEventMap : " + sqsEventMap);
-
-
         Message message = Message.builder()
             .body(sqsMessage.getBody())
             .messageAttributes(sqsMessage.getMessageAttributes().entrySet().stream().collect(
                 Collectors.toMap(Entry::getKey,
                     entry -> toMessageAttributeValue(entry.getValue()))))
             .build();
-
         MessageContent requestMessage = MessageContent.fromMessage(message);
 
         log.log(Level.INFO, "message : " + message.toString());
@@ -71,7 +58,7 @@ public class MainHandler implements RequestHandler<SQSEvent, String> {
 //        log.log(Level.INFO, "message.messageAttributes() " + message.messageAttributes().toString());
 
         Object routerResponse  = router(message);
-        log.log(Level.INFO, "routerreponse: " + routerResponse);
+        log.log(Level.INFO, "routerreponse: " + routerResponse.toString());
 
         MessageContent response = new MessageContent(sqsMessage.getBody());
 
@@ -86,14 +73,9 @@ public class MainHandler implements RequestHandler<SQSEvent, String> {
             AudioOptionDto audioOptionD = new AudioOptionDto(1.0f, 0.0f, 100);
             TtsSentenceDto ttsSentenceDto = new TtsSentenceDto("안녕하세요", voiceD, audioOptionD);
             TtsMakeRequest ttsMakeRequest = new TtsMakeRequest(ttsSentenceDto, "lambdaTest");
-
-
             TtsMakeResponse ttsMakeResponse = ttsMakeService.makeTtsAndUploadS3(ttsMakeRequest);
-
             return ttsMakeResponse;
         }
-
-
         throw new IllegalArgumentException("Unknown message type: " + messageType);
     }
 
